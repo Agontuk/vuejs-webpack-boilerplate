83dcc267fc6a63f0792c491547256270fe479a2a
Iftekhar Rifat <rifat662@gmail.com>
2016-07-21 22:04:57 +0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="app">
+        <img class="logo" src="./assets/logo.png">
+        <hello></hello>
+    </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+    components: {
+        Hello
+    }
+}
+</script>
